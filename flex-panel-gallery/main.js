088880e05d6a7eb48fed2689d20ce03571c3a476
Yauhen Vaznishchyk <evgen.vozn@gmail.com>
2021-01-27 02:43:54 +0300

    fix: corrections

--- flex-panel-gallery/main.js
+++ flex-panel-gallery/main.js
@@ -1,12 +1,11 @@
 const panels = document.querySelectorAll('.panel');
 
 function toggleOpen() {
-  console.log('Hello');
-  this.classList.toggle('open');
-  }
+  panels.forEach(panel => panel.classList.remove("open"));
+  this.classList.add("open");
+}
 
 function toggleActive(e) {
-  console.log(e.propertyName);
   if (e.propertyName.includes('flex')) {
     this.classList.toggle('open-active');
     }
